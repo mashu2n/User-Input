96e65e15d1dc84661096adcff2b0293c4f6d5dc8
Gerrin Murray <50505367+mashu2n@users.noreply.github.com>
2022-07-18 07:31:28 -0500

    added user input and card

---
+++ src/Components/User/AddUser.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+import Card from "../UI/Card";
+import classes from "./AddUser.module.css";
+
+const AddUser = (props) => {
+  const AddUserHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <Card className={classes.input}>
+      <form onSubmit={AddUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input input="username" type={"text"} />
+        <label htmlFor="age">Age(years)</label>
+        <input input="age" type={"number"} />
+        <button type="submit">Add User</button>
+      </form>
+    </Card>
+  );
+};
+
+export default AddUser;
