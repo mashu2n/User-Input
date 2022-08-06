19978781551b53df4ce4c039267a29153c454f43
Gerrin Murray <50505367+mashu2n@users.noreply.github.com>
2022-08-05 20:22:19 -0500

    managing list of Users through state

--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -4,23 +4,19 @@ import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "./AddUser.module.css";
 
-// Fetching user input
 const AddUser = (props) => {
   const [enteredUsername, setEnteredUsername] = useState("");
   const [enteredAge, setEnteredAge] = useState("");
 
-  const AddUserHandler = (event) => {
+  const addUserHandler = (event) => {
     event.preventDefault();
-
-    // validating
     if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
       return;
     }
     if (+enteredAge < 1) {
       return;
     }
-    console.log(enteredUsername, enteredAge);
-    // resetting UI
+    props.onAddUser(enteredUsername, enteredAge);
     setEnteredUsername("");
     setEnteredAge("");
   };
@@ -35,18 +31,18 @@ const AddUser = (props) => {
 
   return (
     <Card className={classes.input}>
-      <form onSubmit={AddUserHandler}>
+      <form onSubmit={addUserHandler}>
         <label htmlFor="username">Username</label>
         <input
-          input="username"
-          type={"text"}
+          id="username"
+          type="text"
           value={enteredUsername}
           onChange={usernameChangeHandler}
         />
-        <label htmlFor="age">Age(years)</label>
+        <label htmlFor="age">Age (Years)</label>
         <input
-          input="age"
-          type={"number"}
+          id="age"
+          type="number"
           value={enteredAge}
           onChange={ageChangeHandler}
         />
